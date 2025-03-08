d5e7137ae1a69d3de25c4824b55a8966e7a9b0b2
Aastha Chordia <aasthachordia595@gmail.com>
2025-03-08 19:14:04 +0530

    Create Quack.java sound Quack Quack

---
+++ Quack.java
@@ -0,0 +1,7 @@
+// Defining a class that represents ducks that quack
+public class Quack implements QuackBehavior {
+
+    public void quack() {
+        System.out.println("Quack! Quack!");
+    }
+}
