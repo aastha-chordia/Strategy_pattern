031f3637d4495d3f3f8c4d9c81ec54e6234259aa
Aastha Chordia <aasthachordia595@gmail.com>
2025-03-08 19:02:46 +0530

    DecoyDuck extending Duck

---
+++ DecoyDuck.java
@@ -0,0 +1,12 @@
+//Aastha Chordia
+// 23070126003
+public class DecoyDuck extends Duck {
+    // Defining a Constructor such that it sets behavior dynamically
+    public DecoyDuck() {
+        super("Decoy Duck", 
+              new NoFLy(), // Cannot fly
+              new MuteQuack(), // Cannot quack
+              new Swim(Swim.SwimType.DROWN)); // Drowns
+    }
+
+
