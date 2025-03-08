be5e58806cf9bc0b648167bc582b8baa96b99c10
Aastha Chordia <aasthachordia595@gmail.com>
2025-03-08 19:04:18 +0530

    define an abstract class Duck

---
+++ Duck.java
@@ -0,0 +1,6 @@
+public abstract class Duck {
+    // Defining protected variables for Duck's name and its behaviors
+    protected String name;
+    protected FlyBehavior flyBehavior;
+    protected QuackBehavior quackBehavior;
+    protected SwimBehavior swimBehavior;
