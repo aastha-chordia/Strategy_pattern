e023b9dd7b58cf24fda343d2f58dd3927fdf6e56
Aastha Chordia <aasthachordia595@gmail.com>
2025-03-08 19:20:35 +0530

    Create Squeak sound

---
+++ Squeak.java
@@ -0,0 +1,7 @@
+// Defining a class that represents ducks that squeak 
+public class Squeak implements QuackBehavior {
+
+    public void quack() {
+        System.out.println("This duck squeaks.");
+    }
+}
