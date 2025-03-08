9732a2c8c081323042eb463eb19614bfd980d4a3
Aastha Chordia <aasthachordia595@gmail.com>
2025-03-08 19:13:17 +0530

    Duck which cannot fly

---
+++ NoFly.java
@@ -0,0 +1,7 @@
+// Defining a class that represents ducks that cannot fly 
+public class NoFLy implements FlyBehavior {
+
+    public void fly() {
+        System.out.println("This duck cannot fly.");
+    }
+}
